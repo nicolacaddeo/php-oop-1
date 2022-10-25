100aa79f7ea4c8e8441fe67623fda79b72704ffe
nicolacaddeo <nicola.caddeo58@gmail.com>
2022-10-25 16:26:10 +0200

    added getCaption method to Movies

--- index.php
+++ index.php
@@ -15,18 +15,26 @@
             $this->runtime = $_runtime;
             $this->budget = $_budget;
         }
+
+        public function getCaption($title, $mainGenre, $releaseDate, $budget ) {
+            ?>
+            <p>
+                <?=$title ?> è un film <?=$mainGenre ?> uscito nelle sale il <?=$releaseDate ?> con un budget stimato di circa <?=$budget ?>
+            </p>
+            <?php
+        }
     }
     
     $inception = new Movie("Inception", ['Action', 'Adventure', 'Thriller'], "July 16, 2010", "United States", "2h 28min", "$160,000,000");
     $oblivion = new Movie("Oblivion", ['Action', 'Adventure', 'Sci-Fi'], "April 19, 2013", "United States", "2h 4min", "$120,000,000");
-    /*
+    
     echo '<pre>';
         var_dump($inception);
     echo '</pre>';
     echo '<pre>';
         var_dump($oblivion);
     echo '</pre>';
-    */
+    
 ?>
 
 <!DOCTYPE html>
@@ -41,72 +49,79 @@
 <body>
     <div class="container">
        <div class="movie-card">
-        <ol>
-            <li>
-                <strong>Titolo: </strong>
-                <?= $inception->title ?>
-            </li>
-            <li>
-                <strong>Generi: </strong>
-                <?php
-                    foreach($inception->genres as $genre) {
-                        echo '<span>'. $genre. ' '.'<span>&#183;</span>'. ' '.  '</span>';
-                    }
-                ?>
-            </li>
-            <li>
-                <strong>Data di uscita: </strong>
-                <?= $inception->releaseDate ?>
-            </li>
-            <li>
-                <strong>Nazione di origine: </strong>
-                <?= $inception->originalCountry ?>
-            </li>
-            <li>
-                <strong>Durata: </strong>
-                <?= $inception->runtime ?>
-            </li>
-            <li>
-                <strong>Budget: </strong>
-                <?= $inception->budget ?>
-            </li>
-        </ol>
+           <ol>
+               <li class="movie-title">
+                   <strong>Titolo: </strong>
+                   <?= $inception->title ?>
+               </li>
+               <li>
+                   <strong>Generi: </strong>
+                   <?php
+                       foreach($inception->genres as $genre) {
+                           echo '<span>'. $genre. ' '.'<span>&#183;</span>'. ' '.  '</span>';
+                       }
+                   ?>
+               </li>
+               <li>
+                   <strong>Data di uscita: </strong>
+                   <?= $inception->releaseDate ?>
+               </li>
+               <li>
+                   <strong>Nazione di origine: </strong>
+                   <?= $inception->originalCountry ?>
+               </li>
+               <li>
+                   <strong>Durata: </strong>
+                   <?= $inception->runtime ?>
+               </li>
+               <li>
+                   <strong>Budget: </strong>
+                   <?= $inception->budget ?>
+               </li>
+           </ol>
+           <?php
+               $inception->getCaption("Inception", "Action", "July 16, 2010", "$160,000,000");
+           ?>
+          </div>
+          <div class="movie-card">
+          <ol>
+               <li>
+                   <strong>Titolo: </strong>
+                   <?= $oblivion->title ?>
+               </li>
+               <li>
+               <strong>Generi: </strong>
+                   <?php
+                       foreach($oblivion->genres as $genre) {
+                           echo '<span>'. $genre. ' '.'<span>&#183;</span>'. ' '.  '</span>';
+                       }
+                   ?>
+               </li>
+               <li>
+                   <strong>Data di uscita: </strong>
+                   <?= $oblivion->releaseDate ?>
+               </li>
+               <li>
+                   <strong>Nazione di origine: </strong>
+                   <?= $oblivion->originalCountry ?>
+               </li>
+               <li>
+                   <strong>Durata: </strong>
+                   <?= $oblivion->runtime ?>
+               </li>
+               <li>
+                   <strong>Budget: </strong>
+                   <?= $oblivion->budget ?>
+               </li>
+           </ol>
+           <?php
+               $oblivion->getCaption("Oblivion", "Action", "April 19, 2013", "$120,000,000");
+           ?>
+          </div> 
        </div>
-       <div class="movie-card">
-       <ol>
-            <li>
-                <strong>Titolo: </strong>
-                <?= $oblivion->title ?>
-            </li>
-            <li>
-            <strong>Generi: </strong>
-                <?php
-                    foreach($oblivion->genres as $genre) {
-                        echo '<span>'. $genre. ' '.'<span>&#183;</span>'. ' '.  '</span>';
-                    }
-                ?>
-            </li>
-            <li>
-                <strong>Data di uscita: </strong>
-                <?= $oblivion->releaseDate ?>
-            </li>
-            <li>
-                <strong>Nazione di origine: </strong>
-                <?= $oblivion->originalCountry ?>
-            </li>
-            <li>
-                <strong>Durata: </strong>
-                <?= $oblivion->runtime ?>
-            </li>
-            <li>
-                <strong>Budget: </strong>
-                <?= $oblivion->budget ?>
-            </li>
-        </ol>
-       </div> 
-    </div>
-</body>
-</html>
+   </body>
+   </html>
+       
 
 
 
